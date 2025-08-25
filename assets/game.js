f8b11348b2819c5e31c5ad0d57f75f1de9543469
Nuvorsan <146627329+nuvorlike@users.noreply.github.com>
2025-08-25 15:37:59 +0700

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -88,7 +88,7 @@ const generateGame = () => {
         'assets/images/image3.jpg',
         'assets/images/image4.jpg',
         'assets/images/image5.jpg',
-        'assets/images/image6.jpg',
+        'assets/images/image6.png',
         'assets/images/image7.jpg',
         'assets/images/image8.png',
         'assets/images/image9.jpg',
