4475359ca7fe694820c0e48e3dd0129bd7b304b4
Nuvorsan <146627329+nuvorlike@users.noreply.github.com>
2025-08-25 15:24:31 +0700

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -84,14 +84,14 @@ const generateGame = () => {
     // Menggunakan path gambar lokal sesuai permintaan Anda.
     // Pastikan path ini benar relatif terhadap file HTML Anda.
     const images = [
-        'assets/images/image1.jpg',
+        'assets/images/image1.png',
         'assets/images/image2.jpg',
         'assets/images/image3.jpg',
         'assets/images/image4.jpg',
         'assets/images/image5.jpg',
         'assets/images/image6.jpg',
         'assets/images/image7.jpg',
-        'assets/images/image8.jpg',
+        'assets/images/image8.png',
         'assets/images/image9.jpg',
         'assets/images/image10.png'
     ];
